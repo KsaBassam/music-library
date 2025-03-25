55920ae85ab36d7ef037bdba87493f29c380f611
Josh Puhala <60090042+JoshuaPuhala@users.noreply.github.com>
2025-03-25 05:31:12 +0000

    Update ShuffleIterator.java

--- model/ShuffleIterator.java
+++ model/ShuffleIterator.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
-import java.util.NoSuchElementException; // Import added
+import java.util.NoSuchElementException; 
 
 public class ShuffleIterator implements Iterator<Song> {
     private List<Song> shuffledList;
